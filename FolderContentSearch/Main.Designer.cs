a69df6bf9e35025a87296715b158f409252a1342
Rashmin Javiya <rashmin.javiya@siemens.com>
2019-10-17 22:55:28 +0530

    feat(App): Initial commit

---
+++ FolderContentSearch/Main.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace FolderContentSearch
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnStartSearch = new System.Windows.Forms.Button();
+            this.txtTextToSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listFiles = new System.Windows.Forms.TreeView();
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmbFileTypeFilter = new System.Windows.Forms.ComboBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolderPath.Location = new System.Drawing.Point(272, 34);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(485, 23);
+            this.txtFolderPath.TabIndex = 1;
+            // 
+            // btnStartSearch
+            // 
+            this.btnStartSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStartSearch.Location = new System.Drawing.Point(889, 34);
+            this.btnStartSearch.Name = "btnStartSearch";
+            this.btnStartSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnStartSearch.TabIndex = 3;
+            this.btnStartSearch.Text = "Start";
+            this.btnStartSearch.UseVisualStyleBackColor = true;
+            this.btnStartSearch.Click += new System.EventHandler(this.btnStartSearch_Click);
+            // 
+            // txtTextToSearch
+            // 
+            this.txtTextToSearch.Location = new System.Drawing.Point(60, 34);
+            this.txtTextToSearch.Name = "txtTextToSearch";
+            this.txtTextToSearch.Size = new System.Drawing.Size(186, 23);
+            this.txtTextToSearch.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "in";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Search";
+            // 
+            // listFiles
+            // 
+            this.listFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listFiles.Location = new System.Drawing.Point(12, 63);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.Size = new System.Drawing.Size(1033, 558);
+            this.listFiles.TabIndex = 8;
+            this.listFiles.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.listFiles_NodeMouseClick);
+            this.listFiles.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listFiles_MouseUp);
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(61, 4);
+            this.contextMenu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenu_ItemClicked);
+            // 
+            // cmbFileTypeFilter
+            // 
+            this.cmbFileTypeFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbFileTypeFilter.FormattingEnabled = true;
+            this.cmbFileTypeFilter.Items.AddRange(new object[] {
+            "All file types",
+            "Only Text Files",
+            "Only CS Files"});
+            this.cmbFileTypeFilter.Location = new System.Drawing.Point(763, 34);
+            this.cmbFileTypeFilter.Name = "cmbFileTypeFilter";
+            this.cmbFileTypeFilter.Size = new System.Drawing.Size(120, 23);
+            this.cmbFileTypeFilter.TabIndex = 10;
+            this.cmbFileTypeFilter.Text = "All file types";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 12);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1033, 10);
+            this.progressBar.TabIndex = 11;
+            this.progressBar.Visible = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(970, 34);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 633);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.cmbFileTypeFilter);
+            this.Controls.Add(this.listFiles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTextToSearch);
+            this.Controls.Add(this.btnStartSearch);
+            this.Controls.Add(this.txtFolderPath);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "Folder Content Search";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btnStartSearch;
+        private System.Windows.Forms.TextBox txtTextToSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView listFiles;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ComboBox cmbFileTypeFilter;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ FolderContentSearch/Main.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FolderContentSearch
+{
+    public partial class FrmMain : Form
+    {
+        #region ::  Variable & Properties  ::
+
+        private readonly ToolStripMenuItem _mnNotepadOpen;
+        private readonly ToolStripMenuItem _mnExpand;
+        private readonly ToolStripMenuItem _mnCollapse;
+        private readonly ToolStripMenuItem _mnExpandAll;
+        private readonly ToolStripMenuItem _mnFolderOpen;
+
+        private string textFileExtensions = "*.txt, *.config, ";
+        private string codeFileExtensions = "*.cs";
+
+        private bool _forceClose = false;
+
+        private bool isInProgress = false;
+
+        #endregion
+
+        #region ::  Constructor  ::
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            _mnNotepadOpen = new ToolStripMenuItem
+            {
+                Name = "mnNotepadOpen",
+                Size = new System.Drawing.Size(181, 22),
+                Text = "Open in Notepad++"
+            };
+
+            _mnExpand = new ToolStripMenuItem
+            {
+                Name = "mnExpand",
+                Size = new System.Drawing.Size(180, 22),
+                Text = "Expand children"
+            };
+
+            _mnExpandAll = new ToolStripMenuItem
+            {
+                Name = "mnExpandAll",
+                Size = new System.Drawing.Size(180, 22),
+                Text = "Expand All"
+            };
+
+            _mnCollapse = new ToolStripMenuItem
+            {
+                Name = "mnCollapse",
+                Size = new System.Drawing.Size(180, 22),
+                Text = "Collapse children"
+            };
+
+            _mnFolderOpen = new ToolStripMenuItem
+            {
+                Name = "mnFolderOpen",
+                Size = new System.Drawing.Size(180, 22),
+                Text = "Explore"
+            };
+
+            contextMenu.Items.AddRange(new ToolStripItem[]
+            {
+                _mnFolderOpen,
+                _mnExpandAll,
+                _mnExpand,
+                _mnCollapse,
+                _mnNotepadOpen
+            });
+        }
+
+        #endregion
+
+        #region ::  Methods   ::
+
+        public void StartSearch(string dirPath)
+        {
+            var selectedFileFilter = "";
+            cmbFileTypeFilter.PerformSafely(() => selectedFileFilter = cmbFileTypeFilter.Text);
+
+            string[] files;
+
+            if (selectedFileFilter == "Only CS Files")
+                files = Directory.GetFiles(dirPath, codeFileExtensions, SearchOption.AllDirectories);
+            else if (selectedFileFilter == "Only Text Files")
+                files = Directory.GetFiles(dirPath, textFileExtensions, SearchOption.AllDirectories);
+            else
+                files = Directory.GetFiles(dirPath, "*.*", SearchOption.AllDirectories);
+
+            files = files.Where(file => Path.GetExtension(file) != ".dll").Select(f => f).ToArray();
+
+            progressBar.PerformSafely(() =>
+            {
+                progressBar.Minimum = 0;
+                progressBar.Maximum = files.Length;
+                progressBar.Value = 0;
+            });
+
+            for (var i = 0; i < files.Length; i++)
+            {
+                if (_forceClose || !isInProgress)
+                {
+                    Thread.CurrentThread.Abort();
+                }
+
+                try
+                {
+                    if (Path.GetExtension(files[i]) == ".dll")
+                        continue;
+
+                    UpdateProgress(i);
+
+                    string fileContent = File.ReadAllText(files[i]);
+                    if (fileContent.ToLower().Contains(txtTextToSearch.Text.ToLower()))
+                    {
+                        PopulateTreeView(listFiles, files[i], '\\', contextMenu);
+                    }
+                }
+                catch (IOException)
+                {
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+        }
+
+        private void UpdateProgress(int value)
+        {
+            progressBar.PerformSafely(() => progressBar.Value = value);
+        }
+
+        private void ChangeControlState(bool state)
+        {
+            isInProgress = !state;
+            btnStartSearch.PerformSafely(() => btnStartSearch.Text = !state ? "Stop" : "Start");
+            progressBar.PerformSafely(() => progressBar.Visible = !state);
+        }
+
+        private void PopulateTreeView(TreeView treeView, string path, char pathSeparator, ContextMenuStrip contextMenu)
+        {
+            TreeNode lastNode = null;
+            var subPathAgg = string.Empty;
+
+            foreach (string subPath in path.Split(pathSeparator))
+            {
+                subPathAgg += subPath + pathSeparator;
+                TreeNode[] nodes = null;
+                treeView.PerformSafely(() => nodes = treeView.Nodes.Find(subPathAgg, true));
+                if (nodes.Length == 0)
+                {
+                    if (lastNode == null)
+                        treeView.PerformSafely(() => lastNode = treeView.Nodes.Add(subPathAgg, subPath));
+                    else
+                        treeView.PerformSafely(() => lastNode = lastNode.Nodes.Add(subPathAgg, subPath));
+                }
+                else
+                    lastNode = nodes[0];
+            }
+        }
+
+        #endregion
+
+        #region ::  Control Events   ::
+
+        private void btnStartSearch_Click(object sender, EventArgs e)
+        {
+            if (isInProgress)
+            {
+                ChangeControlState(true);
+                return;
+            }
+
+            // Validations
+            string selectedPath = txtFolderPath.Text;
+
+            if (!Directory.Exists(selectedPath))
+            {
+                MessageBox.Show("Invalid folder path");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtTextToSearch.Text))
+            {
+                MessageBox.Show("Invalid search text");
+                return;
+            }
+
+            var threadStart = new ThreadStart(delegate
+            {
+                try
+                {
+                    ChangeControlState(false);
+                    StartSearch(selectedPath);
+                }
+                finally
+                {
+                    ChangeControlState(true);
+                }
+            });
+
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            string copiedPath = Clipboard.GetText();
+
+            if (Directory.Exists(copiedPath))
+                txtFolderPath.Text = copiedPath;
+        }
+
+        private void contextMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem.Name == "mnNotepadOpen")
+            {
+                Process.Start("notepad++", listFiles.SelectedNode.FullPath);
+            }
+            else if (e.ClickedItem.Name == "mnExpand")
+            {
+                listFiles.SelectedNode.Expand();
+            }
+            else if (e.ClickedItem.Name == "mnExpandAll")
+            {
+                listFiles.SelectedNode.ExpandAll();
+            }
+            else if (e.ClickedItem.Name == "mnCollapse")
+            {
+                listFiles.SelectedNode.Collapse();
+            }
+            else if (e.ClickedItem.Name == "mnFolderOpen")
+            {
+                Process.Start("explorer.exe", listFiles.SelectedNode.FullPath);
+            }
+        }
+
+        private void listFiles_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+            }
+        }
+
+        private void listFiles_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                // Select the clicked node
+                listFiles.SelectedNode = listFiles.GetNodeAt(e.X, e.Y);
+
+                if (listFiles.SelectedNode != null)
+                {
+                    _mnNotepadOpen.Visible = listFiles.SelectedNode.GetNodeCount(true) == 0;
+                    _mnFolderOpen.Visible = listFiles.SelectedNode.GetNodeCount(true) != 0;
+
+                    _mnExpand.Visible = listFiles.SelectedNode.GetNodeCount(true) != 0 &&
+                                        !listFiles.SelectedNode.IsExpanded;
+                    _mnExpandAll.Visible = listFiles.SelectedNode.GetNodeCount(true) != 0 &&
+                                           !listFiles.SelectedNode.IsExpanded;
+                    _mnCollapse.Visible = listFiles.SelectedNode.GetNodeCount(true) != 0 &&
+                                          listFiles.SelectedNode.IsExpanded;
+                    _mnExpand.Visible = listFiles.SelectedNode.GetNodeCount(true) != 0 &&
+                                        !listFiles.SelectedNode.IsExpanded;
+
+                    contextMenu.Show(listFiles, e.Location);
+                }
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            _forceClose = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listFiles.Nodes.Clear();
+        }
+
+        #endregion
+    }
+}
